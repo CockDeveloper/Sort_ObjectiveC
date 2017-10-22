7d9e3c99b7c3350efa34e2397e89b3f3bfdf7600
Cock.Developer <cock.developer@gmail.com>
2017-10-22 23:43:19 +0700

    Create Project

---
+++ Sort/Sort/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sort
+//
+//  Created by Nguyễn Trọng Anh Tuấn on 22/10/2017.
+//  Copyright © 2017 Nguyễn Trọng Anh Tuấn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
